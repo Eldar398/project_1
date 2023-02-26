dc63eae92b19c6cc46802bd9b9f9a4cc41512eed
Sergey Stepanets <stepanetssergey@gmail.com>
2023-02-27 01:06:25 +0200

    change app.py for database connection

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__, static_url_path='/static')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Gfhjkm12345'
+app.config['MYSQL_DB'] = 'test'
+
+mysql = MySQL(app)
+#object for database connection
 
 
 @app.route('/')
@@ -31,24 +38,37 @@ def signup():
         favorit_language = request.form['fav_language']
         fname = request.form['fname']
         lname = request.form['lname']
-        python_know = request.form['python_know']
-        return render_template('signup.html', method='POST', fav_lang = favorit_language, fname=fname,lname=lname,python_know=python_know)
+        python_know = favorit_language
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO signup(login,password) VALUES(%s,%s)''', (fname, lname))
+        mysql.connection.commit()
+        cursor.close()
+        return render_template('signup.html', method='POST', fav_lang = favorit_language, fname=fname,lname=lname)
     else:
         return render_template('signup.html', method='GET')
 
-@app.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        return render_template('login.html', method='POST', username=username, password=password)
-    else:
-        return render_template('login.html', method='GET')
+@app.route('/users')
+def users():
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' select * from signup ''')
+    user_list = cursor.fetchall()
+    cursor.close()
+    result_users = []
+    #tuple - []
+    for item in user_list:
+        result_users.append({'login': item[1], 'password': item[2]})
+    return result_users
+
+# TODO
+# 1. login route and add login to header
+# 2. check if password is correct
+# 3. if it is correct change login to logout
+# 4. not correct show some message (your password is not correct)
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
